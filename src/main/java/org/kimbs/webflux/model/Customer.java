8ab84b19dd0d2505d27f603449c8a5f820740a9c
kimbs <vlakd0711@naver.com>
2019-03-29 16:48:13 +0900

    customer model 제약조건 추가

--- src/main/java/org/kimbs/webflux/model/Customer.java
+++ src/main/java/org/kimbs/webflux/model/Customer.java
@@ -3,6 +3,8 @@ package org.kimbs.webflux.model;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
+import org.hibernate.validator.constraints.Length;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,11 +16,13 @@ public class Customer {
 
     private Long id;
 
+    @Length(min = 1, max = 20, message = "should be length between 1 and 20")
     private String firstname;
 
+    @Length(min = 1, max = 20, message = "should be length between 1 and 20")
     private String lastname;
 
-    @Min(value = 1)
-    @Max(value = 99)
+    @Min(value = 1, message = "should be greater than equal to 1")
+    @Max(value = 99, message = "should be less than equal to 99")
     private int age;
 }
